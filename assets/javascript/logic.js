dd03246685ab0184c1169894a23c4d637b0d97e1
arnold <arnoldrz2@gmail.com>
2017-12-16 13:06:42 -0600

    added photo function to JS

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,10 @@
-// Testing Google APIs
-
+// Function that replaces spaces with "%20" in the address string
 function URLify (string) {
   return string.trim().replace(/\s/g, '%20');
   }
 
-
-
+// Function that pulls address info from HTML address input boxes
+// It also appends senator/representative information to the appropriate boxes
 $("#submit").on("click", function(event) {
   event.preventDefault();
 
@@ -14,24 +13,19 @@ $("#submit").on("click", function(event) {
     //$('#house').val('');
 
 
-
       // Google Civic API
       var apikey = "AIzaSyCzbYDzSuQiuisiSRNcgt1JPATAXVEgsAY";
 
-      //Address format: "1700%20North%201st%20Street%20San%20Jose%2095112"
-      //"https://www.googleapis.com/civicinfo/v2/voterinfo?address=78+Tarkiln+Hill+Road%2C+New+Bedford%2C+Massachusetts+02745&key=buifwGbuibfw&_79521Dh89fwafb"
+      // Address format Example: "1700%20North%201st%20Street%20San%20Jose%2095112"
+      // Store individual input values as variables and trim extra spaces
       var street = $("#address").val().trim();
       var street2 = URLify(street);
       var city = $("#city").val().trim();
       var zipcode = $("#zipcode").val().trim();
-      //var streetForm = street.text(JSON.stringify(obj));
-
+      // Compile value variables into one string with "%20" between each variable
       var address = street2 + "%20" + city + "%20" + zipcode;
 
-      // var queryURL = "https://www.googleapis.com/civicinfo/v2/voterinfo?address=78+Tarkiln+Hill+Road%2C+New+Bedford%2C+Massachusetts+02745&key=buifwGbuibfw&_79521Dh89fwafb";
-
-      //var address = "5506%20Grover%20Ave%20Austin%2078756"
-
+      // Create queryURL with compiled values, apikey, and other necessary text
       var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?" + "address=" + address + "&key=" + apikey;
 
 
@@ -48,6 +42,9 @@ $("#submit").on("click", function(event) {
        $("#senator").append(response.officials[2].name);
        $("#senator").append(response.officials[3].name);
 
+       $("#senator1url").attr("src", response.officials[2].photoUrl);
+       $("#senator2url").attr("src", response.officials[3].photoUrl);
+
        $("#senator1url").attr("href", response.officials[2].urls);
        $("#senator2url").attr("href", response.officials[3].urls);
 
@@ -56,6 +53,9 @@ $("#submit").on("click", function(event) {
 
        //Set website and info for House of Represenatives to the webpage
        $("#representative").append(response.officials[8].name);
+
+       $("#housereppic").attr("src", response.officials[8].name);
+
        $("#houseurl").attr("href", response.officials[8].urls);
 
        $("#houseurl").text(response.officials[8].name + " Website");
