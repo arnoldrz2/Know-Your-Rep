ba5ef3c196fbd623eadac2df854f71efc61e1160
arnold <arnoldrz2@gmail.com>
2017-12-17 14:52:10 -0600

    reverting index and logic files to previous state from 12/16/12

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -46,42 +46,26 @@ $("#submit").on("click", function(event) {
        $("#senator1name").text("Name: " + response.officials[2].name);
        $("#senator2name").text("Name: " + response.officials[3].name);
 
-
        $("#senator1party").text("Party: " + response.officials[2].party);
        $("#senator2party").text("Party: " + response.officials[3].party);
 
-       $("#senator1party").append("<a id="senator1url" href="" target="_blank"></a>");
-       $("#senator2party").append("<a id="senator2url" href="" target="_blank"></a>");
+       //$("#senator1url").attr("src", response.officials[2].photoUrl);
+       //$("#senator2url").attr("src", response.officials[3].photoUrl);
 
        $("#senator1url").attr("href", response.officials[2].urls);
        $("#senator2url").attr("href", response.officials[3].urls);
 
-
        $("#senator1url").text(response.officials[2].name + " Website");
        $("#senator2url").text(response.officials[3].name + " Website");
 
-       $("#senator1Phone").text("Phone Number: " + response.officials[2].phones[0]);
-       $("#senator2Phone").text("Phone Number: " + response.officials[3].phones[0]);
-
-       $("#senator1Twitter").text("Twitter Handle:" + response.officials[2].channels[1]);
-       $("#senator2Twitter").text("Twitter Handle:" + response.officials[3].channels[1]);
-
-       $("#senator1FB").text("Facebook Page:" + response.officials[2].channels[0]);
-       $("#senator2FB").text("Facebook Page:" + response.officials[3].channels[0]);
-
        //Set website and info for House of Represenatives to the webpage
-       $("#representative").text("Name: " + response.officials[8].name);
+       $("#representative").append(response.officials[8].name);
 
        $("#housereppic").attr("src", response.officials[8].name);
 
        $("#houseurl").attr("href", response.officials[8].urls);
 
        $("#houseurl").text(response.officials[8].name + " Website");
-
-       $("#housePhone").text("Phone Number: " + response.officials[8].phones[0]);
-       $("#houseTwitter").text("Twitter Handle:" + response.officials[8].channels[0]);
-       $("#houseFB").text("Facebook Page:" + response.officials[2].channels[1]);
-
 
       });
     });
